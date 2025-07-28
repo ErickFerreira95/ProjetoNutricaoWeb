15b528d018afadcd7060d212fe2e3b0d97454879
Erick Ferreira <erick_designer7@hotmail.com>
2025-07-28 12:36:22 -0300

    Criação cadastrarUsuarioView

--- src/main/java/com/mycompany/ProjetoNutricaoWeb/controller/LoginController.java
+++ src/main/java/com/mycompany/ProjetoNutricaoWeb/controller/LoginController.java
@@ -7,7 +7,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class LoginController {
     
     @GetMapping("/")
-    public String ola(){
+    public String getLoginView(){
     return "login";
+    }
+
+    @GetMapping("/cadastrarUsuario")
+    public String getCadastrarUsuarioView(){
+        return "cadastrarUsuario";
     }
 }
